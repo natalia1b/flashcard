e42a55e29c8c96afd1c39c6ebad9e29d7c3a8d32
natalia1b <148780999+natalia1b@users.noreply.github.com>
2024-10-15 16:05:13 -0300

    62746547

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ function criaCartao(categoria, pergunta, resposta) {
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
-     </div>
-<div class="cartao__conteudo__resposta">
-     <p>${resposta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
      </div>
      </div>
      `
