32985d9f9d8c1eae6d83505dd371f3597b30a8a7
natalia1b <148780999+natalia1b@users.noreply.github.com>
2024-11-05 15:40:42 -0300

    834783ghdvbhaegh

--- perguntas.js
+++ perguntas.js
@@ -28,10 +28,4 @@ criaCartao(
     'É o conjunto de traços observáveis ou que podem ser mensurados de um indivíduo.'
 )
 
-criaCartao(
-    'Biologia',
-    'A Segunda Lei de Mendel, também chamada de lei da segregação independente, diz que os fatores para duas ou mais características segregam-se de maneira independente, distribuindo-se para os gametas e recombinando-se ao acaso. De acordo com essa lei, podemos concluir que um indivíduo de genótipo BBCc terá gametas:
-
-?',
-    'BC e Bc.'
-)
+c
