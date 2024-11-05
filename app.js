c17d38daff5eff3e2fac7fb15272bad1e15251bf
natalia1b <148780999+natalia1b@users.noreply.github.com>
2024-11-05 16:13:11 -0300

    wefgsdhsrhyf

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
-     </div>
-     </div>
-     `
+    </div>
+    </div>
+    `
 
-     let respostaEstaVisivel = false
+    let respostaEstaVisivel = false
 
-     function viraCartao() {
+    function viraCartao() {
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
-     }
-     cartao.addEventListener('click', viraCartao)
-
-
-     container.appendChild(cartao)
-
     }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
+}
